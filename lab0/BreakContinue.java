c3dca3b652b5368a27fc148429d3cfb836176b08
Tam Le <tamhle824@gmail.com>
2022-01-02 19:38:09 -0600

    completed lab 0

---
+++ lab0/BreakContinue.java
@@ -0,0 +1,22 @@
+public class BreakContinue {
+    public static void windowPosSum(int[] a, int n) {
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] < 0) {
+                continue;
+            } else {
+                for(int j = 1; j < n+1;j++){
+                    if (i+j >= a.length){
+                        break;
+                    }else
+                        a[i] += a[i+j];
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] a = {1, 2, -3, 4, 5, 4};
+        int n = 3;
+        windowPosSum(a,n);
+        System.out.println(java.util.Arrays.toString(a));
+    }
+}
